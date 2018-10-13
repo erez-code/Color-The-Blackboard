ee76d9719605ef71a0d33d51ca686f900768a2b6
erez-code <41819488+erez-code@users.noreply.github.com>
2018-10-13 18:43:21 +0300

    Color-The-Blackboard

---
+++ coloreverything.js
@@ -0,0 +1,15 @@
+$(".content").click(function() {
+	if ($(this).css("background-color") == "rgb(0, 0, 0)") {
+		$(this).css("background-color", colorWell.value)} else {
+  	$(this).css("background-color", "black")}})
+
+var colorWell;
+var defaultColor = "#2098df";
+
+window.addEventListener("load", startup, false);
+
+function startup() {
+  colorWell = document.querySelector("#colorWell");
+  colorWell.value = defaultColor;
+  colorWell.select();
+}
