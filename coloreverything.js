cb73a1b7b84bab6a14b70131240cca829d01dca5
erez-code <41819488+erez-code@users.noreply.github.com>
2018-10-23 23:01:33 +0300

    Add files via upload

--- coloreverything.js
+++ coloreverything.js
@@ -12,3 +12,14 @@ $( function() {
 	colorSelect.value = defaultColor;
 	colorSelect.select();
 } );
+
+$("#clearcanvas").click(function(event)
+  { 
+    $(".wrap div").css("background-color", "black")
+  })
+
+$("#randomcanvas").click(function(event)
+  { 
+    for (var i=0; i<4000; i++) {
+	$(".wrap div").eq(i).css("background-color", '#'+ Math.round(0xffffff * Math.random()).toString(16))}
+  })
