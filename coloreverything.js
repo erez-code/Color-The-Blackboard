3e19de19da58e3d27a97aefe46ff04189c6cf4a5
erez-code <41819488+erez-code@users.noreply.github.com>
2018-10-24 10:11:14 +0300

    Update coloreverything.js

--- coloreverything.js
+++ coloreverything.js
@@ -16,10 +16,8 @@ $( function() {
 $("#clearcanvas").click(function(event)
   { 
     $(".wrap div").css("background-color", "black")
-  })
+});
 
-$("#randomcanvas").click(function(event)
-  { 
-    for (var i=0; i<4000; i++) {
-	$(".wrap div").eq(i).css("background-color", '#'+ Math.round(0xffffff * Math.random()).toString(16))}
-  })
+function randomColors() { return '#'+ Math.round( 0xffffff * Math.random() ).toString( 16 ) };
+
+$( '.wrap div' ).css( 'background-color', randomColors );
